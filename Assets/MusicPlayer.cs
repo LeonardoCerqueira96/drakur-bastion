c73fdc57423ccf9f809c03b839a07c9a38350c70
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2018-05-31 00:53:34 -0300

    Circuit waypoints added

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour 
+{
+    private static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(gameObject);
+            created = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start() 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		
+	}
+}
---
+++ Assets/SplashTransition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashTransition : MonoBehaviour 
+{
+    [SerializeField] private float loadDelay = 5f;
+
+	// Use this for initialization
+	void Start() 
+	{
+        Invoke("EndSplashScreen", loadDelay);
+	}
+
+    private void EndSplashScreen()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
