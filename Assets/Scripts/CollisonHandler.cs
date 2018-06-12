59ed76450dbccdbc637616ce1601d9c96a1ced41
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2018-06-12 14:06:16 -0300

    Disabling controls on player death

---
+++ Assets/Scripts/CollisonHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisonHandler : MonoBehaviour 
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        SendMessage("OnPlayerDeath");
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,39 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour 
+public class PlayerController : MonoBehaviour 
 {
-    [Tooltip("In ms^-1")] [SerializeField] private float xSpeed = 3f;
-    [Tooltip("In ms^-1")] [SerializeField] private float ySpeed = 3f;
-
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] private float horizontalSpeed = 3f;
+    [Tooltip("In ms^-1")] [SerializeField] private float verticalSpeed = 3f;
     [Tooltip("In meters")] [SerializeField] private float xRange = 5f;
     [Tooltip("In meters")] [SerializeField] private float yRange = 4f;
 
+    [Header("Screen position based")]
     [SerializeField] private float positionPitchFactor = -4f;
-    [SerializeField] private float controlPitchFactor = -30f;
-
     [SerializeField] private float positionYawFactor = 4f;
 
+    [Header("Control axis based")]
+    [SerializeField] private float controlPitchFactor = -30f;
     [SerializeField] private float controlRollFactor = -20f;
 
     private float xThrow, yThrow;
-
-    // Use this for initialization
-    void Start() 
-	{
-		
-	}
-
-    private void OnTriggerEnter(Collider collider)
-    {
-        Debug.Log("Player triggered something");
-    }
+    private bool isControlEnabled = true;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        CalculateTranslation();
-        CalculateRotation();
+        if (isControlEnabled)
+        {
+            CalculateTranslation();
+            CalculateRotation();
+        }
     }
 
     private void CalculateTranslation()
@@ -49,7 +43,7 @@ public class Player : MonoBehaviour
     private float GetNewXPosition()
     {
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xSpeed * xThrow * Time.deltaTime;
+        float xOffset = horizontalSpeed * xThrow * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
@@ -59,7 +53,7 @@ public class Player : MonoBehaviour
     private float GetNewYPosition()
     {
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        float yOffset = ySpeed * yThrow * Time.deltaTime;
+        float yOffset = verticalSpeed * yThrow * Time.deltaTime;
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
@@ -73,5 +67,16 @@ public class Player : MonoBehaviour
         float roll = controlRollFactor * xThrow;
 
         transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void OnPlayerDeath() // called by string reference
+    {
+        DisableControls();
+    }
+
+    private void DisableControls()
+    {
+        isControlEnabled = false;
+        Debug.Log("Controls disabled");
     }
 }
