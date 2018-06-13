b606dd08de08345f99e2689e6e7ed3f3f3edfb3c
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2018-06-12 21:22:34 -0300

    Explosing FX and Reload on death

--- Assets/Scripts/CollisonHandler.cs
+++ Assets/Scripts/CollisonHandler.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisonHandler : MonoBehaviour 
 {
+    [Tooltip("In seconds")] [SerializeField] private float reloadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] private GameObject deathFX;
+
+
     private void OnTriggerEnter(Collider collider)
     {
         StartDeathSequence();
@@ -12,5 +17,12 @@ public class CollisonHandler : MonoBehaviour
     private void StartDeathSequence()
     {
         SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("ReloadLevel", reloadDelay);
+    }
+
+    private void ReloadLevel() // string referenced
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
